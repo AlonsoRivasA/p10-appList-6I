9a0b618f35c77c5184d9fdf11d658641b500626c
LUIS_ALONSO_RIVAS_ANDRADE <a.21308051280398@cbtis128.edu.mx>
2024-02-28 16:34:13 +0000

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiListaCard());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiListaCard extends StatelessWidget {
+  const MiListaCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "App Listview Rivas",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: PaginaInicio(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class PaginaInicio extends StatefulWidget {
+  const PaginaInicio({Key? key}) : super(key: key);
 
+  @override
+  State<PaginaInicio> createState() => _PaginaInicioState();
+}
+
+class _PaginaInicioState extends State<PaginaInicio> {
+  List<String> images = [
+    "assets/images/xbox1.jpg",
+    "assets/images/xbox2.jpg",
+    "assets/images/xbox3.png",
+    "assets/images/xbox4.png",
+    "assets/images/xbox5.jpg",
+    "assets/images/xbox6.png",
+    "assets/images/xbox7.png",
+    "assets/images/xbox8.jpg",
+    "assets/images/xbox9.jpg",
+    "assets/images/xbox10.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("ListView Alonso Rivas"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext, index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(images[index]),
+              ),
+              title: Text("Xbox"),
+              subtitle: Text("Xbox logos"),
+            ),
+          );
+        },
+        itemCount: images.length,
+        shrinkWrap: true,
+        padding: EdgeInsets.all(5),
+        scrollDirection: Axis.vertical,
       ),
     );
   }
